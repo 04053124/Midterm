d595e07a1cef0d3d7517ab122735c11ef984d543
04053124 <ljm0850529@gmail.com>
2017-04-17 19:27:02 +0800

    Add files via upload

---
+++ mid3.ino
@@ -0,0 +1,39 @@
+
+#include <SevSeg.h>
+#include <Keypad.h>
+SevSeg sevseg; 
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("7 04053124 04053106");
+   byte numDigits =4;
+   byte digitPins[] ={8,9,12,13};
+   byte segmentPins[] = {6,3,10,11,4,7,5};
+   sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
+  
+}
+
+void loop() 
+{
+ 
+   int val,n=0,x;
+    if (Serial.available()) 
+    { 
+         val = Serial.parseInt();
+         if(val<=9999 && val >=0)
+          {
+            Serial.println(val);
+            sevseg.setNumber(val);
+            sevseg.refreshDisplay();
+          }
+     
+        else 
+       {
+            Serial.println("range error");
+       }
+    }   
+   else
+   sevseg.refreshDisplay();
+   
+   
+   }
